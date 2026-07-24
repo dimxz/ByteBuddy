1e5b0c7242c4dc5f3fd169cee0f87f5650dfe902
dimxz <adimas.said.511@gmail.com>
2026-07-24 21:00:47 +0700

    fix: bot reply only available for sender

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.command("/bytebuddy-ping", async ({ command, ack, respond }) => {
   const start = Date.now();
   await ack();
   const latency = Date.now() - start;
-  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+  await respond({ text: `Pong!\nLatency: ${latency}ms`, response_type: 'in_channel'});
 });
 
 // BASE64 UTILITY
@@ -21,20 +21,16 @@ app.command("/bytebuddy-base64", async ({ command, ack, respond }) => {
     await ack();
   const mode = command.text.split(' ')[0];
   const input = command.text.split(' ').slice(1).join(' ');
-
-  console.log(mode);
-  console.log(input);
-  
   
   if (mode !== 'encode' && mode !== 'decode'){
     await respond({ text: `Enter valid 'encode' or 'decode' mode.` });
     } else {
     if (mode == 'decode') {
       const decoded = validateAndDecode(input, 'base64');
-      await respond({ text: `Decoded strings:\n${decoded}` });
+      await respond({ text: `Decoded strings:\n${decoded}`, response_type: 'in_channel' });
     } else if (mode == 'encode') {
       const encoded = Buffer.from(input, 'utf-8').toString('base64');
-      await respond({ text: `Encoded strings:\n${encoded}` });
+      await respond({ text: `Encoded strings:\n${encoded}`, response_type: 'in_channel'});
   }
     }
 });
@@ -51,13 +47,13 @@ app.command("/bytebuddy-url", async ({ command, ack, respond }) => {
     if (mode == 'decode') {
       try {
         const decoded = decodeURIComponent(input);
-        await respond({ text: `Decoded URL:\n${decoded}` });
+        await respond({ text: `Decoded URL:\n${decoded}`, response_type: 'in_channel' });
       } catch (error) {
-        await respond({ text: `Invalid URL-encoded string.`})
+        await respond({ text: `Invalid URL-encoded string.`, response_type: 'in_channel'})
       }
     } else if (mode == 'encode') {
       const encoded = encodeURIComponent(input);
-      await respond({ text: `Encoded URL:\n${encoded}` });
+      await respond({ text: `Encoded URL:\n${encoded}`, response_type: 'in_channel' });
   }
     }
 });
@@ -69,7 +65,7 @@ app.command("/bytebuddy-hextorgb", async ({ command, ack, respond }) => {
   
   if (input.length == 3) {
     const doubled = input.split('').map(char => parseInt(char + char, 16));
-    await respond({ text: `RGB Value:\nR : ${doubled[0]}\nG : ${doubled[1]}\nB : ${doubled[2]}` });
+    await respond({ text: `RGB Value:\nR : ${doubled[0]}\nG : ${doubled[1]}\nB : ${doubled[2]}`, response_type: 'in_channel' });
 
   } else if (input.length == 6) {
     const chunks = []
@@ -77,7 +73,7 @@ app.command("/bytebuddy-hextorgb", async ({ command, ack, respond }) => {
     chunks.push(parseInt(input.slice(i, i + 2), 16));
   }
     
-    await respond({ text: `RGB Value:\nR : ${chunks[0]}\nG : ${chunks[1]}\nB : ${chunks[2]}` });
+    await respond({ text: `RGB Value:\nR : ${chunks[0]}\nG : ${chunks[1]}\nB : ${chunks[2]}`, response_type: 'in_channel' });
   } else {
     await respond({ text: `Invalid input length.` });  
   }
@@ -86,5 +82,5 @@ app.command("/bytebuddy-hextorgb", async ({ command, ack, respond }) => {
 
 (async () => {
   await app.start();
-  console.log("bot is running!");
+  console.log("ByteBuddy is now running!");
 })();
